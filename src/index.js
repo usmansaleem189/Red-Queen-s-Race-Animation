53e6cb154a052177a1c8009390d15da0bb951f4e
usmansaleem189 <62993998+usmansaleem189@users.noreply.github.com>
2020-07-16 23:31:41 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import {App} from './App';
 
 ReactDOM.render(
 //   <React.StrictMode>
-    <App />,
+    <React>
+    <App />
+    </React>,
 //   </React.StrictMode>,
   document.getElementById('root')
 );
